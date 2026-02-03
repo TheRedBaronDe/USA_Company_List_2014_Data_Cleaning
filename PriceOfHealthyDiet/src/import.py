c0f6a0608d7ebfbdeffa2eddc5d06db679d1f0f7
Victoria Silva <vicsilva.de@gmail.com>
2026-02-03 11:30:30 -0300

    Add import statements for data manipulation and analysis

---
+++ PriceOfHealthyDiet/src/import.py
@@ -0,0 +1,11 @@
+# Data manipulation
+import pandas as pd
+import numpy as np 
+
+# Visualization
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+# Statistical analysis
+from scipy import stats
+from scipy.stats import pearsonr, spearmanr
