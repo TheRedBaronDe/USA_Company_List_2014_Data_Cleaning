dea5e0491df645c62fe2ae3f8271bc3957b3a087
Victoria Silva <vicsilva.de@gmail.com>
2026-02-10 14:58:03 -0300

    Add SQL queries for data validation checks
    Added queries to check for null values, unique years, and duplicate entries in the dataset.

--- TwoCenturiesOfUMRaces/sql/queries.sql
+++ TwoCenturiesOfUMRaces/sql/queries.sql
@@ -32,4 +32,55 @@ SELECT COUNT(*) FROM public."TwoCenturiesOfUMRaces"; -- count all the values imp
 SELECT * FROM public."TwoCenturiesOFUMRaces" 
 LIMIT 5; -- show the first five rows
 
--- Check for null values
+-- Check for null values in the most important columns
+
+SELECT
+    COUNT(*) FILTER (WHERE year_of_event IS NULL) AS null_year,
+    COUNT(*) FILTER (WHERE event_name IS NULL) AS null_event,
+    COUNT(*) FILTER (WHERE athlete_id IS NULL) AS null_athlete,
+    COUNT(*) FILTER (WHERE athlete_gender IS NULL) AS null_gender,
+    COUNT(*) FILTER (WHERE athlete_country IS NULL) AS null_country,
+    COUNT(*) FILTER (WHERE athlete_average_speed IS NULL) AS null_avg_speed
+FROM public."TwoCenturiesOfUMRaces";
+
+-- Check the available years
+
+SELECT DISTINCT year_of_event
+FROM public."TwoCenturiesOfUMRaces"
+ORDER BY year_of_event; -- this query returns 146 different values (years)
+
+-- Check how many unique countries the dataset has
+
+SELECT COUNT(DISTINCT athlete_country)
+FROM public."TwoCenturiesOfUMRaces"; -- this query returns 209 distinct values (countries)
+
+-- Checks if an athlete was recorded more than once on the same event and on the same year
+
+SELECT
+    year_of_event,
+    event_name,
+    athlete_id,
+    COUNT(*)
+FROM public."TwoCenturiesOfUMRaces"
+GROUP BY
+    year_of_event,
+    event_name,
+    athlete_id
+HAVING COUNT(*) > 1; -- it returns various values, but that's expected since we're dealing with athletes racing on very long marathons
+
+-- Check for real duplicates by comparing the athlete's performance and checking if all rows look identical
+
+SELECT
+    COUNT(*) AS total_rows,
+    COUNT(DISTINCT (
+        year_of_event,
+        event_name,
+        athlete_id,
+        athlete_performance,
+        athlete_average_speed
+    )) AS distinct_rows
+FROM public."TwoCenturiesOfUMRaces";
+
+-- it returned:    Total Rows: 7,461,195 - Distinct Rows: 7,460,935
+-- a 260 difference in values, meaning there are 260 identical (most likely duplicated) values
+-- this makes up for 0.00348469649% of the total dataset, making it insignificant
