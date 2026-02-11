12396d9c786e2da5d897d7a13564b496df1ae219
Victoria Silva <vicsilva.de@gmail.com>
2026-02-11 09:45:28 -0300

    Update and rename 1.import.py to 1-import.py

--- PriceOfHealthyDiet/src/1-import.py
+++ PriceOfHealthyDiet/src/1-import.py
@@ -11,14 +11,14 @@ from scipy import stats
 from scipy.stats import pearsonr, spearmanr
 
 # Set visualization style
-plt.style.use('seaborn-v0_8-darkgrid')
-sns.set_palette("husl")
+plt.style.use('seaborn-v0_8-darkgrid') # apply a predefined style to the plot
+sns.set_palette("husl") # set a colour
 
 # Display settings
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', 100)
-pd.set_option('display.float_format', '{:.2f}'.format)
+pd.set_option('display.max_columns', None) # displays all the columns
+pd.set_option('display.max_rows', 100) # displays up to 100 rows
+pd.set_option('display.float_format', '{:.2f}'.format) # round floats up to two decimal places
 
 # Load the cleaned dataset
 df = pd.read_csv('C:\\Users\\User\\Documents\\PostgreSQL\\price_of_healthy_diet_clean.csv')
-print(df.head()) # Print first rows
+print(df.head()) # prints first rows
